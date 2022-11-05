d7f8a5b8b8900ede7df87e89edd167a63a4b9bab
lucas Ramos <lucasramos.lr260@gmail.com>
2022-11-04 21:23:43 -0300

    Adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Números pares entre 0 e ? ");
+int NumeroInformado = Convert.ToInt32(Console.ReadLine());
+if (NumeroInformado >= 1)
+{
+    int NumeroPar = 0;
+    while (NumeroPar <= NumeroInformado)
+    {
+        Console.Write($"{NumeroPar} ");
+        NumeroPar += 2;
+    }
+}
+else
+{
+    Console.WriteLine($"O número {NumeroInformado} não é inteiro positivo. ");
+}
